4766fa1da5a586713185800625df763d1ff3107d
aidarminyazev <legodark@mail.ru>
2025-02-24 03:07:12 +0500

    Add MovementInfo schema and MovementInfoResponse for enhanced data representation

--- src/schemas/movement.py
+++ src/schemas/movement.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel
 from datetime import datetime
 
+from src.schemas.response import BaseResponse
+
 class MovementBase(BaseModel):
     movement_id: str
     event: str
@@ -15,3 +17,15 @@ class MovementDB(MovementBase):
 
     class Config:
         from_attributes = True
+        
+class MovementInfo(MovementBase):
+    movement_id: str
+    departure_from: str | None = None
+    arrival_at: str | None = None
+    departure_time: str | None = None
+    arrival_time: str | None = None
+    duration: str | None = None
+    quantity_difference: int | None = None
+    
+class MovementInfoResponse(BaseResponse):
+    payload: MovementInfo
