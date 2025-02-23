44ffcc5c471935b260866725fd69f04680bb8c8b
aidarminyazev <legodark@mail.ru>
2025-02-24 04:32:48 +0500

    Refactor project structure by removing main.py, adding config.py for environment variables, and updating database connection in database.py

--- src/database/database.py
+++ src/database/database.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.asyncio import (
     AsyncSession,
     AsyncConnection,
 )
-from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
+from src.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
