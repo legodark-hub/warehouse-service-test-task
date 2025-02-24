8fe4046506fa06274463e182f5132a701af9aa7f
aidarminyazev <legodark@mail.ru>
2025-02-24 05:26:51 +0500

    Remove change_product_quantity method from MovementService class

--- src/api/services/movement.py
+++ src/api/services/movement.py
@@ -1,4 +1,3 @@
-from datetime import timedelta
 from src.utils.service import BaseService
 from src.utils.uow import transaction_mode
 
@@ -57,22 +56,4 @@ class MovementService(BaseService):
         movement = await self.uow.movement.add_movement(movement_data)
         return movement
     
-    @transaction_mode
-    async def change_product_quantity(self, movement_message: dict):
-        warehouse_id = movement_message["data"]["warehouse_id"]
-        product_id = movement_message["data"]["product_id"]
-        quantity = movement_message["data"]["quantity"]
-        event = movement_message["event"]
-
-        current_quantity = await self.uow.warehouse.get_quantity(warehouse_id, product_id)
-
-        if current_quantity is None:
-            if event == "arrival":
-                await self.uow.warehouse.add_product(warehouse_id, product_id, quantity)
-            else:
-                raise ValueError(f"Product with ID {product_id} does not exist in warehouse with ID {warehouse_id}")
-        else:
-            change_amount = quantity if event == "arrival" else -quantity
-            if event == "departure" and current_quantity < quantity:
-                raise ValueError(f"Insufficient quantity in warehouse: current quantity is {current_quantity}, requested quantity is {quantity}")
-            await self.uow.warehouse.change_quantity(warehouse_id, product_id, change_amount)
+    
