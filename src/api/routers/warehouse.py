c97d75fce985d0679ce9dbef595b8fbf705e200f
aidarminyazev <legodark@mail.ru>
2025-02-24 05:54:21 +0500

    Update get_product_quantity endpoint to return WarehouseQuantityResponse model

--- src/api/routers/warehouse.py
+++ src/api/routers/warehouse.py
@@ -1,15 +1,19 @@
 from fastapi import APIRouter
 from fastapi import Depends
+from src.schemas.warehouse import WarehouseQuantityResponse
 from src.api.services.warehouse import WarehouseService
 
 
 router = APIRouter(prefix="/warehouses")
 
 
-@router.get("/{warehouse_id}/products/{product_id}", response_model={"quantity": int})
+@router.get(
+    "/{warehouse_id}/products/{product_id}", response_model=WarehouseQuantityResponse
+)
 async def get_product_quantity(
     warehouse_id: str,
     product_id: str,
     service: WarehouseService = Depends(WarehouseService),
 ):
-    return await service.get_product_quantity(warehouse_id, product_id)
+    quantity = await service.get_product_quantity(warehouse_id, product_id)
+    return WarehouseQuantityResponse(payload=quantity)
