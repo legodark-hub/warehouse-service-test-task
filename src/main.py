20522b1b300c3fd793a9578f5acf6c5cce148d80
Aidar Minyazev <54939061+legodark-hub@users.noreply.github.com>
2025-04-28 14:20:23 +0500

    Revert "Refactor main application to include movement and warehouse routers, and update Kafka consumer integration"
    This reverts commit 33a3897f12466929a65d73999b9439039b835d13.

--- src/main.py
+++ src/main.py
@@ -1,11 +1,8 @@
 from fastapi import FastAPI
+
 from faststream.kafka.fastapi import KafkaRouter
-from src.api.routers.movement import router as movement_router
-from src.api.routers.warehouse import router as warehouse_router
-from src.consumers.movement import kafka_router as consumer_router
+
+kafka_router = KafkaRouter("localhost:9092")
 
 app = FastAPI()
-
-app.include_router(consumer_router)
-app.include_router(movement_router)
-app.include_router(warehouse_router)
+app.include_router(kafka_router)
