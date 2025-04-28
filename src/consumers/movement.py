30c1625f2419a47b14a1e2e4075aeb33f8e95028
Aidar Minyazev <54939061+legodark-hub@users.noreply.github.com>
2025-04-28 14:20:23 +0500

    Revert "Integrate WarehouseService to process product quantity changes in movement response handling"
    This reverts commit bf6fe5a85ff8c1ed6f2bb90864c4d45b8143471c.

--- src/consumers/movement.py
+++ src/consumers/movement.py
@@ -2,22 +2,18 @@ from fastapi import Depends
 from faststream.kafka.fastapi import Logger
 
 from src.api.services.movement import MovementService
-from src.api.services.warehouse import WarehouseService
 from src.main import kafka_router
 from src.schemas.movement import MovementMessage
-
 
 @kafka_router.subscriber("response")
 async def hello_response(
     m: MovementMessage,
     logger: Logger,
-    movement_service: MovementService = Depends(MovementService),
-    warehouse_service: WarehouseService = Depends(WarehouseService),
+    service: MovementService = Depends(MovementService),
 ):
-    logger.info("Message received")
-    try:
-        await movement_service.save_movement_info(m)
-        await warehouse_service.change_product_quantity(m)
-        logger.info("Message processed")
-    except Exception as e:
-        logger.error(f"Error processing message: {e}")
+    logger.info(m)
+    await service.save_movement_info(m)
+    
+
+    
+    
